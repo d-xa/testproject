158083e8e99639bf66776303cc81d8c818dc8cac
anh <anhdo@gmx.net>
2021-09-06 15:13:41 +0200

    add testing

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
+
 """
 This is a simple Hello Python script
 """
-print('Hello Python')
+
+def hello():
+    """ function say hello """
+    return 'Hello Python'
+
+
+if __name__ == '__main__':
+    print(hello())
---
+++ test_hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import unittest
+from hello import *
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = hello()
+        b = 'Hello Python'
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
